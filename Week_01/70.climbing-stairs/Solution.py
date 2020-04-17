29cadcec0cd231580276933f07678a562cdde5aa
chenchuxin <idesireccx@163.com>
2020-04-18 00:05:55 +0800

    climbing-stairs

---
+++ Week_01/70.climbing-stairs/Solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+
+        f1, f2 = 1, 2
+        for i in range(3, n + 1):
+            f1, f2 = f2, f1 + f2;
+        return f2;
